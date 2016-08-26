3ebe1c98a19fe234367fba5e0cae118bb0427a21
Paul Nelson <pnelson@greendotcorp.com>
2016-08-25 21:58:53 -0400

    update provisioning scripts

--- provision/backup.sh
+++ provision/backup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ansible-playbook -vvv -i ramnode.inventory backup.yml
+ansible-playbook -vvv -i main.inventory backup.yml
+#ansible-playbook -vvv -i ramnode_test.inventory backup.yml
--- provision/setup_user.sh
+++ provision/setup_user.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -vvv -i ramnode_test.inventory --extra-vars "username=$(whoami)" --ask-pass setup_user.yml
+ansible-playbook -vvv -i inventory/main.inventory --extra-vars "username=$(whoami)" -e "ansible_ssh_user=ec2-user" --private-key ~/Downloads/zombie_wp_keypair.pem  setup_user.yml --limit $1
