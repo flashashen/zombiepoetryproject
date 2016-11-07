16a519626558366420bebb02b5a7e1640ff61448
Paul Nelson <pnelson@greendotcorp.com>
2016-11-07 13:08:40 -0500

    latest ans

--- provision/provision.sh
+++ provision/provision.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #ansible-playbook -vvv -i ramnode_test.inventory host_zombie_wp.yml
-ansible-playbook -vvv -i inventory/main.inventory host_zombie_wp.yml --limit $1
+ansible-playbook -vvv -i inventory/main.inventory --ask-vault-pass host_zombie_wp.yml --limit $1
--- provision/setup_user.sh
+++ provision/setup_user.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -vvv -i inventory/main.inventory --extra-vars "username=$(whoami)" -e "ansible_ssh_user=ec2-user" --private-key ~/Downloads/zombie_wp_keypair.pem  setup_user.yml --limit $1
+ansible-playbook -vvv -i inventory/main.inventory --extra-vars "username=$(whoami)" -e "ansible_ssh_user=ec2-user" -e 'host_key_checking=False'  --private-key ~/.ssh/default_ec2.pem --ask-vault-pass setup_user.yml --limit $1
