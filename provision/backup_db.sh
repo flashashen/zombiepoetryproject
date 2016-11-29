00c49bb0fef74b114b5453939b744a55c9431b36
Paul Nelson <pnelson@greendotcorp.com>
2016-11-28 20:45:33 -0500

    update ansible stuff. add dsh config file.

--- provision/backup_db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -vvv -i inventory/main.inventory backup_db.yml --ask-vault-pass --limit=52.39.104.229
-#50.112.10.226
--- provision/set_hostname.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -vvv -i inventory/main.inventory zombie_sethostname.yml --ask-vault-pass --limit $1
